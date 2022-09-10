d4fb0c0ea7582cd475ecf1b3589039a571b0bd45
Sameer Khan <112516825+smrkhan21@users.noreply.github.com>
2022-09-11 00:06:16 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,71 @@
+// search app
+import { useEffect, useState } from "react";
+import List from "./List";
+import "./styles.css";
+
+const App = () => {
+  const [searchTerm, setSearchTerm] = useState("");
+  const [ListItems, setListItems] = useState(List);
+
+  useEffect(() => {
+    setListItems(
+      searchTerm !== ""
+        ? List.filter((item) =>
+            item.name.toLowerCase().includes(searchTerm.toLocaleLowerCase())
+          )
+        : List
+    );
+  }, [searchTerm]);
+  return (
+    <>
+      <h2 style={{ textAlign: "center" }}>Search App</h2>
+      <div
+        className="searchInput"
+        style={{ textAlign: "center", fontSize: "20px" }}
+      >
+        <label>
+          Search:
+          <span>
+            <input
+              type="text"
+              placeholder="Enter Text to Search"
+              value={searchTerm}
+              onChange={(e) => setSearchTerm(e.target.value)}
+            />
+          </span>
+        </label>
+        <p style={{ textAlign: "center", fontWeight: 600 }}>
+          {ListItems.length} records found
+        </p>
+      </div>
+      <table>
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>NAME</th>
+          </tr>
+        </thead>
+        {ListItems.length ? (
+          <tbody>
+            {ListItems.map((listItem) => {
+              return (
+                <tr key={listItem.id}>
+                  <td>{listItem.id}</td>
+                  <td>{listItem.name}</td>
+                </tr>
+              );
+            })}
+          </tbody>
+        ) : (
+          <tr>
+            <td colSpan="2" style={{ textAlign: "center" }}>
+              No Record Found!
+            </td>
+          </tr>
+        )}
+      </table>
+    </>
+  );
+};
+
+export default App;
---
+++ src/List.js
@@ -0,0 +1,131 @@
+const List = [
+  {
+    id: 1,
+    name: "HTML"
+  },
+  {
+    id: 2,
+    name: "CSS"
+  },
+  {
+    id: 3,
+    name: "JAVASCRIPT"
+  },
+  {
+    id: 4,
+    name: "BOOTSTRAP"
+  },
+  {
+    id: 5,
+    name: "REACT JS"
+  },
+  {
+    id: 6,
+    name: "PHP"
+  },
+  {
+    id: 7,
+    name: "PYTHON"
+  },
+  {
+    id: 8,
+    name: "MAGENTO"
+  },
+  {
+    id: 9,
+    name: "MYSQL"
+  },
+  {
+    id: 10,
+    name: "DJANGO"
+  },
+  {
+    id: 11,
+    name: "LARAVEL"
+  },
+  {
+    id: 12,
+    name: "CODEIGNITER"
+  },
+  {
+    id: 13,
+    name: "NODE JS"
+  },
+  {
+    id: 14,
+    name: "ANGULAR JS"
+  },
+  {
+    id: 15,
+    name: "EXPRESS JS"
+  },
+  {
+    id: 16,
+    name: "GRAPHQL"
+  },
+  {
+    id: 17,
+    name: "RESTAPI"
+  },
+  {
+    id: 18,
+    name: "AWS"
+  },
+  {
+    id: 19,
+    name: "SHOPIFY"
+  },
+  {
+    id: 20,
+    name: "WORDPRESS"
+  },
+  {
+    id: 21,
+    name: "MONGODB"
+  },
+  {
+    id: 22,
+    name: "PHALCON"
+  },
+  {
+    id: 23,
+    name: "BLOCKCHAIN"
+  },
+  {
+    id: 24,
+    name: "WEB3"
+  },
+  {
+    id: 25,
+    name: "MACHINE LEARNING"
+  },
+  {
+    id: 26,
+    name: "DATA SCIENCE"
+  },
+  {
+    id: 27,
+    name: "C++"
+  },
+  {
+    id: 28,
+    name: "JAVA"
+  },
+  {
+    id: 29,
+    name: "NEXT JS"
+  },
+  {
+    id: 30,
+    name: "GITHUB"
+  },
+  {
+    id: 31,
+    name: "JIRA"
+  },
+  {
+    id: 32,
+    name: "GITLAB"
+  }
+];
+export default List;
